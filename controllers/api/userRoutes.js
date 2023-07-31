9a8360e61177f16fa6ee277ac5584efb0654807a
erbester51 <55757768+erbester51@users.noreply.github.com>
2023-07-31 12:32:54 -0500

    Updated userRoutes

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/', async (req, res) => {
+
+});
+
+router.post('/login', async (req, res) => {
+
+});
+
+router.post('/logout', async (req, res) => {
+
+})
